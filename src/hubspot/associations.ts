d70b634750d9d5ae304c382665f4f82bd0cb3a2f
unknown <umeshbilagi@gmail.com>
2022-11-20 21:24:09 +0530

    done

--- src/hubspot/associations.ts
+++ src/hubspot/associations.ts
@@ -1,36 +1,54 @@
 // import hubspot from "@hubspot/api-client"
-const hubspot = require('@hubspot/api-client')
-import { hubspotObject, arrayHubspotObject, hubspotAssocitionType } from "./types";
+const hubspot = require("@hubspot/api-client");
+import {
+  hubspotObject,
+  arrayHubspotObject,
+  hubspotAssocitionType,
+} from "./types";
 
-export default class Associations{
-    private hapiKey
-    constructor(hapiKey = process.env.HUBSPOT_API_KEY){
-        this.hapiKey = hapiKey;
+export default class Associations {
+  private hapiKey;
+  constructor(hapiKey = process.env.HUBSPOT_API_KEY) {
+    this.hapiKey = hapiKey;
+  }
+
+  async create(config: {
+    fromObjectType: hubspotObject;
+    toObjectType: hubspotObject;
+    fromObjectId: any;
+    toObjectId: any;
+  }): Promise<any> {
+    const curAssocitionType = `${
+      arrayHubspotObject.filter((el) => el.type === config.fromObjectType)[0]
+        .single
+    }_to_${
+      arrayHubspotObject.filter((el) => el.type === config.toObjectType)[0]
+        .single
+    }`;
+
+    const hubspotClient = new hubspot.Client({ accessToken: this.hapiKey });
+
+    const batchInputPublicAssociation = {
+      inputs: [
+        {
+          from: { id: config.fromObjectId },
+          to: { id: config.toObjectId },
+          type: curAssocitionType,
+        },
+      ],
+    };
+    const fromObjectType = config.fromObjectType;
+    const toObjectType = config.toObjectType;
+    try {
+      const apiResponse = await hubspotClient.crm.associations.batchApi.create(
+        fromObjectType,
+        toObjectType,
+        batchInputPublicAssociation
+      );
+    } catch (e: any) {
+      e.message === "HTTP request failed"
+        ? console.error(JSON.stringify(e.response, null, 2))
+        : console.error(e);
     }
-
-    async create(config : {
-        fromObjectType : hubspotObject;
-        toObjectType : hubspotObject;
-        fromObjectId: any;
-        toObjectId : any
-    }):Promise<any>{
-
-            const curAssocitionType = `${arrayHubspotObject.filter(el=> el.type === config.fromObjectType)[0].single}_to_${arrayHubspotObject.filter(el=> el.type === config.toObjectType)[0].single}`
-   
-            const hubspotClient = new hubspot.Client({ accessToken:this.hapiKey});
-
-            const batchInputPublicAssociation = { inputs: [{"from":{"id": config.fromObjectId},"to":{"id": config.toObjectId},"type":curAssocitionType}] };
-            const fromObjectType = config.fromObjectType;
-            const toObjectType = config.toObjectType;
-                try {
-                const apiResponse = await hubspotClient.crm.associations.batchApi.create(fromObjectType, toObjectType, batchInputPublicAssociation);
-                
-                } catch (e:any) {
-                e.message === 'HTTP request failed'
-                    ? console.error(JSON.stringify(e.response, null, 2))
-                    : console.error(e)
-                }
-            
-
-    }
+  }
 }
--- src/hubspot/hubspot-objects.ts
+++ src/hubspot/hubspot-objects.ts
@@ -1,5 +1,5 @@
 // import hubspot from "@hubspot/api-client"
-const hubspot = require('@hubspot/api-client')
+const hubspot = require("@hubspot/api-client");
 import axios from "axios";
 import { hubspotObject, hubspotOparetor } from "./types";
 
@@ -78,35 +78,50 @@ export default class HubspotObjects {
     }
   }
 
-      /**
-       *
-       * @param config
-       * @returns
-       */
-    async getByPropertyName(config : {objects:hubspotObject, propertyName: string, value: any, operator:hubspotOparetor, properties:[], limit:number}): Promise<any> {
-      const hubspotClient = new hubspot.Client({ accessToken:this.hapiKey});
-      let options = {
-          "filterGroups": [{ "filters": [{ propertyName: config.propertyName, "operator": config.operator as string, "value": config.value }] }],
-          "properties": config.properties,
-          "limit" : config.limit
-      }
+  /**
+   *
+   * @param config
+   * @returns
+   */
+  async getByPropertyName(config: {
+    objects: hubspotObject;
+    propertyName: string;
+    value: any;
+    operator: hubspotOparetor;
+    properties: [];
+    limit: number;
+  }): Promise<any> {
+    // const hubspotClient = new hubspot.Client({ accessToken:this.hapiKey});
+    let options = {
+      filterGroups: [
+        {
+          filters: [
+            {
+              propertyName: config.propertyName,
+              operator: config.operator as string,
+              value: config.value,
+            },
+          ],
+        },
+      ],
+      properties: config.properties,
+      limit: config.limit,
+    };
 
-       // @ts-ignore
-       let apiResponse;
-       if(config.objects === "contacts"){
-           // @ts-ignore
-           apiResponse= await hubspotClient.crm.contacts.searchApi.doSearch(options).then((res:any ) => JSON.parse(JSON.stringify(res.body.results)))
-       }else if(config.objects === "companies"){
-          // @ts-ignore
-          apiResponse=  await hubspotClient.crm.companies.searchApi.doSearch(options).then((res:any ) => JSON.parse(JSON.stringify(res.body.results)))
-       }else if(config.objects === "deals"){
-           // @ts-ignore
-           apiResponse= await hubspotClient.crm.deals.searchApi.doSearch(options).then((res: any) => JSON.parse(JSON.stringify(res.body.results)))
-       }else if(config.objects === "tickets"){
-           // @ts-ignore
-           apiResponse=  await hubspotClient.crm.tickets.searchApi.doSearch(options).then((res: any) => JSON.parse(JSON.stringify(res.body.results)))
-       }
-       return apiResponse;
-
+    let uri = "";
+    let apiResponse;
+    if (config.objects === "contacts") {
+      uri = `https://api.hubapi.com/crm/v3/objects/contacts/search`;
+    } else if (config.objects === "companies") {
+      uri = `https://api.hubapi.com/crm/v3/objects/companies/search`;
+    } else if (config.objects === "deals") {
+      uri = `https://api.hubapi.com/crm/v3/objects/deals/search`;
+    } else if (config.objects === "tickets") {
+      uri = "https://api.hubapi.com/crm/v3/objects/deals/search";
+    }
+    apiResponse = await axios
+      .post(uri, options, this.header)
+      .then((res) => res.data);
+    return apiResponse;
   }
 }
